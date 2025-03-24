300869d768fa402c3e41a62df7e34cbda8760355
Phiroze Noble <fio@Fios-Personal-MacBook-Pro.local>
2025-03-24 11:16:26 -0400

    More clean up

--- src/pages/shared/Sidebar.jsx
+++ src/pages/shared/Sidebar.jsx
@@ -1,6 +1,13 @@
-import { h } from "preact";
 import { useLocation } from "preact-iso";
 import { isAuthenticated, signOut } from "../../services/auth";
+
+import MenuIcon from "../../assets/icons/Menu.svg";
+import LogoutIcon from "../../assets/icons/Logout.svg";
+import LoginIcon from "../../assets/icons/Login.svg";
+import EditorIcon from "../../assets/icons/Editor.svg";
+import TabListIcon from "../../assets/icons/TabList.svg";
+import SettingsIcon from "../../assets/icons/Settings.svg";
+import HelpIcon from "../../assets/icons/Help.svg";
 
 const Sidebar = ({ isOpen, toggleSidebar }) => {
   const { url, route } = useLocation();
@@ -30,20 +37,7 @@ const Sidebar = ({ isOpen, toggleSidebar }) => {
           className="p-1 rounded-md hover:bg-gray-700 focus:outline-none"
           aria-label="Toggle sidebar"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M4 6h16M4 12h16M4 18h16"
-            />
-          </svg>
+          <MenuIcon />
         </button>
       </div>
 
@@ -54,97 +48,28 @@ const Sidebar = ({ isOpen, toggleSidebar }) => {
           isOpen={isOpen}
           label="New Dab"
           isActive={url.includes("/editor#new")}
-          icon={
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M12 4v16m8-8H4"
-              />
-            </svg>
-          }
+          icon={<EditorIcon />}
         />
-
         <NavItem
           href="/"
           isOpen={isOpen}
           label="Tab List"
           isActive={url === "/" || url === ""}
-          icon={
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M4 6h16M4 12h16M4 18h7"
-              />
-            </svg>
-          }
+          icon={<TabListIcon />}
         />
-
         <NavItem
           href="/settings"
           isOpen={isOpen}
           label="Settings"
           isActive={url.includes("/settings")}
-          icon={
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
-              />
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-              />
-            </svg>
-          }
+          icon={<SettingsIcon />}
         />
-
         <NavItem
           href="/help"
           isOpen={isOpen}
           label="Help"
           isActive={url.includes("/help")}
-          icon={
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-              />
-            </svg>
-          }
+          icon={<HelpIcon />}
         />
       </nav>
 
@@ -160,20 +85,7 @@ const Sidebar = ({ isOpen, toggleSidebar }) => {
             <div
               className={`${isOpen ? "mr-4" : ""} transition-transform active:scale-95`}
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
-                />
-              </svg>
+              <LogoutIcon />
             </div>
             {isOpen && (
               <span className="transition-opacity duration-300">Logout</span>
@@ -189,20 +101,7 @@ const Sidebar = ({ isOpen, toggleSidebar }) => {
             <div
               className={`${isOpen ? "mr-4" : ""} transition-transform active:scale-95`}
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"
-                />
-              </svg>
+              <LoginIcon />
             </div>
             {isOpen && (
               <span className="transition-opacity duration-300">Login</span>
