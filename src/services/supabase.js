e60785448ce50a33190dd31ca249ff39712d2846
Fionoble <p.fio.noble@gmail.com>
2025-04-18 08:17:37 -0400

    Pull out env vars

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://zeedkzthmlrpsdlbvazj.supabase.co";
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InplZWRrenRobWxycHNkbGJ2YXpqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI3NDAyNjEsImV4cCI6MjA1ODMxNjI2MX0.dOlrLFs_LmiCj9YOP7-XKAfbGHWgJvmbMVX489ceSoA";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
